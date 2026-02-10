47df53acfaf1e94b4e21f206995b6624eb600092
FreddyR03 <freddy030307@gmail.com>
2026-02-09 20:12:46 -0500

    feat(service): introduce service interface and concrete implementation

--- src/main/java/com/campus/backendproject/service/proveedor/impl/ProveedorImagenHerramientaImpl.java
+++ src/main/java/com/campus/backendproject/service/proveedor/impl/ProveedorImagenHerramientaImpl.java
@@ -1,4 +1,41 @@
 package com.campus.backendproject.service.proveedor.impl;
 
-public class ProveedorImagenHerramientaImpl {
+import com.campus.backendproject.dto.imagenherramienta.ImagenHerramientaRequest;
+import com.campus.backendproject.dto.imagenherramienta.ImagenHerramientaResponse;
+import com.campus.backendproject.entity.ImagenHerramienta;
+import com.campus.backendproject.repository.ImagenHerramientaRepository;
+import com.campus.backendproject.service.proveedor.interfaz.ProveedorImagenHerramientaService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProveedorImagenHerramientaImpl implements ProveedorImagenHerramientaService {
+
+
+    private ImagenHerramientaRepository repository;
+
+    public ProveedorImagenHerramientaImpl(ImagenHerramientaRepository repository){
+        this.repository = repository;
+    }
+    @Override
+    public ImagenHerramientaResponse crearUrl(ImagenHerramientaRequest imagenHerramientaRequest) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Long id) {
+
+    }
+
+    @Override
+    public ImagenHerramientaResponse actualizar(Long id, ImagenHerramientaRequest imagenHerramientaRequest) {
+        return null;
+    }
+
+    private ImagenHerramientaResponse convertirResponse(ImagenHerramienta imagenHerramienta){
+        return new ImagenHerramientaResponse(
+                imagenHerramienta.getId(),
+                imagenHerramienta.getUrlImagen(),
+                imagenHerramienta.getHerramienta() != null ? imagenHerramienta.getHerramienta().getId() : Long.valueOf("Sin Herramienta")
+        );
+    }
 }
--- src/main/java/com/campus/backendproject/service/proveedor/interfaz/ProveedorImagenHerramientaService.java
+++ src/main/java/com/campus/backendproject/service/proveedor/interfaz/ProveedorImagenHerramientaService.java
@@ -1,4 +1,12 @@
 package com.campus.backendproject.service.proveedor.interfaz;
 
+import com.campus.backendproject.dto.imagenherramienta.ImagenHerramientaRequest;
+import com.campus.backendproject.dto.imagenherramienta.ImagenHerramientaResponse;
+
 public interface ProveedorImagenHerramientaService {
+    ImagenHerramientaResponse crearUrl(ImagenHerramientaRequest imagenHerramientaRequest);
+
+    void eliminar(Long id);
+
+    ImagenHerramientaResponse actualizar(Long id, ImagenHerramientaRequest imagenHerramientaRequest);
 }
